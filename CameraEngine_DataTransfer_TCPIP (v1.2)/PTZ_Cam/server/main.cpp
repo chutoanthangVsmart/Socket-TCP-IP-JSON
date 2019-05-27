f20a8554ae3e3c626db399c17ffbec63e90c5595
chutoanthangVsmart <50815659+chutoanthangVsmart@users.noreply.github.com>
2019-05-27 10:03:10 +0700

    Add files via upload

---
+++ CameraEngine_DataTransfer_TCPIP (v1.2)/PTZ_Cam/server/main.cpp
@@ -0,0 +1,44 @@
+#include "tcp_server.h"
+
+int main()
+{
+    int newSd;
+    tcp_server *server;
+    server = new tcp_server();
+    server->serversocket_setup(1111, newSd);
+
+    string respond = "Server received\n";
+
+    while(1)
+    {
+        string data;
+
+        //receive a message from the client (listen)
+        cout << "Awaiting client response..." << endl;
+        data = server->receive(512);
+        if(data == "exit")
+        {
+            cout << "Client has quit the session" << endl;
+            break;
+        }
+        else
+        {
+            cout << "Client: " << data << endl;
+            server->send_data(respond);
+        }
+
+//        {// input data and send
+//        cout << ">";
+//        getline(cin, data);
+//        if(data == "exit")
+//        {
+//            //send to the client that server has closed the connection
+//            server->send_data(data);
+//            break;
+//        }
+//        //send the message to client
+//        server->send_data(data);
+//        }
+    }
+    return 0;
+}
